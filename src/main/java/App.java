57ba40b8b25cc814d723f9b477f8902d011c9f67
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-16 19:36:19 -0400

    Add logic method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,10 +2,16 @@ public class App {
 
     public static void main(String[] args) {
 
+        logic();
     }
 
 
     static  void logic(){
+
+        System.out.println("HERE COMES THE DICE!");
+
+
+
 
     }
 
