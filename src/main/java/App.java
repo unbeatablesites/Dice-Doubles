75cbdacb6daf75c7c1ea6d3db2a62418613c4db2
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-16 19:47:50 -0400

    Add logic method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -10,15 +10,15 @@ public class App {
 
     static  void logic(){
 
-        System.out.println("HERE COMES THE DICE!");
+        System.out.println("HERE COMES THE DICE!\n");
 
         Random rand = new Random();
 
         int randomRoll1 = 1 + rand.nextInt(6);
         int randomRoll2 = 1 + rand.nextInt(6);
 
-        System.out.println("Roll #1:" + randomRoll1 );
-        System.out.println("Roll #2:" + randomRoll2 );
+        System.out.println("Roll #1: " + randomRoll1 );
+        System.out.println("Roll #2: " + randomRoll2 );
 
 
     }
