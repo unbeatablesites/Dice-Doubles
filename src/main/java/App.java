bae4300310c84d4e7f6a6cf4320c238644453b24
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-16 19:42:36 -0400

    Add logic method

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class App {
 
     public static void main(String[] args) {
@@ -10,7 +12,29 @@ public class App {
 
         System.out.println("HERE COMES THE DICE!");
 
+        Random rand = new Random();
 
+        int randomRoll1 = 1 + rand.nextInt(6);
+        int randomRoll2 = 1 + rand.nextInt(6);
+
+        System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2); System.out.println(randomRoll1);
+        System.out.println(randomRoll2);
 
 
     }
