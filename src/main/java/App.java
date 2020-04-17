ed4f066f4104da553faded46d5fc102c6caf2722
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-16 20:03:26 -0400

    Adding counter logic

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -32,7 +32,7 @@ public class App {
 
             System.out.println("Roll #1: " + randomRoll1 );
             System.out.println("Roll #2: " + randomRoll2 );
-            System.out.println("The total is "+ (randomRoll1 + randomRoll2)+"!");
+            System.out.println("The total is "+ (randomRoll1 + randomRoll2)+"!\n");
         }
         System.out.println("It took "+count+" rolls to get doubles\n");
     }
