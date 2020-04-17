d8f788cbf43a86f49dab867a94590235a3c66d42
Frank Uzoka <Frankuzoka@hotmail.com>
2020-04-16 20:07:31 -0400

    All done

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -22,8 +22,7 @@ public class App {
         System.out.println("Roll #1: " + randomRoll1 );
         System.out.println("Roll #2: " + randomRoll2 );
 
-        System.out.println("The total is "+ (randomRoll1 + randomRoll2)+"!");
-        System.out.println(count+"\n");
+        System.out.println("The total is "+ (randomRoll1 + randomRoll2)+"!\n");
 
         while(randomRoll1 != randomRoll2){
             count ++;
